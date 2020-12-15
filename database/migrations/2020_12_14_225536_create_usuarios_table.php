2e9c1cb99fd0b6730efd44fb061252b19e07abd3
rubenerangel <rubenrang@gmail.com>
2020-12-15 01:08:03 -0400

    Simple API

---
+++ database/migrations/2020_12_14_225536_create_usuarios_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 20);
+            $table->string('last_name', 20);
+            $table->string('phone', 20);
+            $table->string('email', 50);
+            $table->string('address', 200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
